3eb04d5e2d5d1974301a3be170679542367fa56b
Олеся <olesia.kazakova.17@yandex.ru>
2024-10-04 15:04:49 +0500

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,219 +1,234 @@
 import static java.lang.System.in;
 import static java.lang.System.out;
-
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
-//через 2 недели сдать отчет на сайте psu.online (к следующей паре)
-//9 вариант ( нечет задания )
-//20 задач каждая задача по 0,4. 2 балла за отчет
-//интерфейс дружественный, чтобы пользователь понимал какую задачу решает программа
-//защита от дурака, проверять входные данные
+
+//сдать отчет на сайте psu.online (к следующей паре)
+//9 вариант (нечет задания)
+
 public class Main {
     public static void main(String[] args) {
         Main n = new Main();
         Scanner in = new Scanner(System.in);
-        //1 ex
+        Random rnd = new Random();
 
-        out.println("Дробная часть числа \nВведите число, дробную часть которого нужно вывести");
-        double x = in.nextDouble();
-        out.println(n.fraction(x));
+        //1(1.1)
+        out.println("Дробная часть числа\nВведите число, дробную часть которого нужно вывести:");
+        if (in.hasNextDouble()) {
+            double x11 = in.nextDouble();
+            out.println(n.fraction(x11));
+        } else {
+            out.println("Неверный ввод");
+        }
 
-        //3 ex
-        /*
-        out.println("Преобразование строки в число \nВведите число");
-        String c0 = in.next();
-        char c= c0.charAt(0);
-        out.println(n.charToNum(c));
-         */
-        //5 ex
-        /*
-        out.println("Введите число");
-        int x = in.nextInt();
-        out.println("Число двузначное?");
-        out.println(n.is2Digits(x));
-         */
-        //7 ex
-        /*
-        out.println("Входит ли число диапазон \nВведите диапазон");
-        int a = in.nextInt();
-        int b = in.nextInt();
-        out.println("Введите число");
-        int num = in.nextInt();
-        out.println(n.isInRange(a,b,num));
-         */
-        //9 ex
-        /*out.println("Введите 3 числa");
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int c = in.nextInt();
-        out.println(n.isEqual(a,b,c));
-         */
-        //2.1 ex
-        /*
+        //2(1.3)
+        out.println("Преобразование буквы в число\nВведите число:");
+        String s13 = in.next();
+        if (s13.length()==1) {
+            char c13 = s13.charAt(0);
+            out.println(n.charToNum(c13));
+        } else {
+            out.println("Неверный ввод");
+        }
+
+        //3(1.5)
+        out.println("Проверка, является ли число двузначным\nВведите число:");
+        if (in.hasNextInt()) {
+            int x15 = in.nextInt();
+            out.println("Число двузначное?");
+            out.println(n.is2Digits(x15));
+        } else out.println("Неверный ввод");
+
+        //4(1.7)
+        out.println("Входит ли число в диапазон\nВведите диапазон:");
+        if (in.hasNextInt()) {
+            int a17 = in.nextInt();
+            if (in.hasNextInt()) {
+                int b17 = in.nextInt();
+                out.println("Введите число:");
+                if (in.hasNextInt()) {
+                    int num17 = in.nextInt();
+                    out.println(n.isInRange(a17, b17, num17));
+                } else out.println("Неверный ввод");
+            } else out.println("Неверный ввод");
+        } else out.println("Неверный ввод");
+
+        //5(1.9)
+        out.println("Равенство трех чисел\nВведите 3 числa:");
+        if (in.hasNextInt()) {
+            int a19 = in.nextInt();
+            if (in.hasNextInt()) {
+                int b19 = in.nextInt();
+                if (in.hasNextInt()) {
+                    int c19 = in.nextInt();
+                    out.println(n.isEqual(a19, b19, c19));
+                } else out.println("Неверный ввод");
+            } else out.println("Неверный ввод");
+        } else out.println("Неверный ввод");
+
+        //6(2.1)
         out.println("Модуль числа\nВведите число:");
-        int a = in.nextInt();
-        out.println(n.abs(a));
-         */
-        //2.3 ex
-        /*
+        if (in.hasNextInt()) {
+            int a21 = in.nextInt();
+            out.println(n.abs(a21));
+        } else out.println("Неверный ввод");
+
+        //7(2.3)
         out.println("Делимость числа на 3 и 5\nВведите число:");
-        int a = in.nextInt();
-        out.println(n.is35(a));
-        */
-        //2.5 ex
-        /*
+        if (in.hasNextInt()) {
+            int a23 = in.nextInt();
+            out.println(n.is35(a23));
+        } else out.println("Неверный ввод");
+
+        //8(2.5)
         out.println("Максимальное число из тройки чисел\nВведите числа:");
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int c = in.nextInt();
-        out.println(n.max3(a,b,c));
-         */
-        //2.7 ex
-        /*out.println("Сумма двух чисел\nВведите числа:");
-        int a = in.nextInt();
-        int b = in.nextInt();
-        out.println(n.sum2(a,b));
-         */
-        //2.9 ex
-        /*
+        if (in.hasNextInt()) {
+            int a25 = in.nextInt();
+            if (in.hasNextInt()) {
+                int b25 = in.nextInt();
+                if (in.hasNextInt()) {
+                    int c25 = in.nextInt();
+                    out.println(n.max3(a25, b25, c25));
+                } else out.println("Неверный ввод");
+            } else out.println("Неверный ввод");
+        } else out.println("Неверный ввод");
+
+        //9(2.7)
+        out.println("Сумма двух чисел\nВведите числа:");
+        if (in.hasNextInt()) {
+            int a27 = in.nextInt();
+            if (in.hasNextInt()) {
+                int b27 = in.nextInt();
+                out.println(n.sum2(a27, b27));
+            } else out.println("Неверный ввод");
+        } else out.println("Неверный ввод");
+
+        //10(2.9)
         out.println("День недели\nВведите число, обозначающее день недели:");
         if (in.hasNextInt()) {
-            int a = in.nextInt();
-            out.println(n.day(a));
-        } else out.println("это не день недели");
-        */
-        //3.1 ex
-        /*
+            int a29 = in.nextInt();
+            out.println(n.day(a29));
+        } else out.println("Это не день недели");
+
+        //11(3.1)
         out.println("Числа от 0 до x\nВведите число x:");
-        if (in.hasNextInt()) {
-            int a = in.nextInt();
-            out.println(n.listNums(a));
+        boolean check31;
+        int a31;
+        if ((check31 = in.hasNextInt()) && (a31 = in.nextInt())>=0) {
+            out.println(n.listNums(a31));
         } else out.println("Неверный ввод");
-         */
-        //3.3 ex
-       /*
+
+        //12(3.3)
         out.println("Четные числа от 0 до x\nВведите число x:");
-        boolean check;
-        int a;
-        if ((check=in.hasNextInt()) && ((a = in.nextInt()) >= 0)) {
-            out.println(n.chet(a));
+        boolean check33;
+        int a33;
+        if ((check33=in.hasNextInt()) && ((a33 = in.nextInt()) >= 0)) {
+            out.println(n.chet(a33));
         } else out.println("Неверный ввод");
-        */
-        //3.5 ex
-        /*
+
+        //13(3.5)
         out.println("Количество знаков в числе\nВведите число:");
         if (in.hasNextInt()) {
-            int a = in.nextInt();
-            out.println(n.numLen(a));
+            int a35 = in.nextInt();
+            out.println(n.numLen(a35));
         } else out.println("Неверный ввод");
-         */
-        //3.7 ex
-        /*
+
+        //14(3.7)
         out.println("Квадрат из \"*\"\nВведите размер стороны квадрата:");
-        boolean check;
-        int a;
-        if ((check=in.hasNextInt()) && ((a = in.nextInt()) > 0)) {
-            n.square(a);
+        boolean check37;
+        int a37;
+        if ((check37=in.hasNextInt()) && ((a37 = in.nextInt()) > 0)) {
+            n.square(a37);
         } else out.println("Неверный ввод");
-         */
-        //3.9 ex
-        /*
+
+        //15(3.9)
         out.println("Треугольник из \"*\"\nВведите высоту:");
-        boolean check;
-        int a;
-        if ((check=in.hasNextInt()) && ((a = in.nextInt()) > 0)) {
-            n.rightTriangle(a);
+        boolean check39;
+        int a39;
+        if ((check39=in.hasNextInt()) && ((a39 = in.nextInt()) > 0)) {
+            n.rightTriangle(a39);
         } else out.println("Неверный ввод");
-         */
-        //4.1 ex
-        /*
-        out.println("Индекс первого вхождения числа x в массив\nЕсли число не входит в массив вернется -1\nВведите x:");
-        int[] array = new int[10];
-        Random rnd = new Random();
+
+        //16(4.1)
+        out.println("Индекс первого вхождения числа x в массив\nЕсли число не входит в массив вернется -1");
+        int[] array41 = new int[10];
         for (int i = 0; i<10; i++) {
-            array[i] = rnd.nextInt(10)+1;
+            array41[i] = rnd.nextInt(10) + 1;
         }
+        out.println("Исxодный массив: " + Arrays.toString(array41));
+        out.println("Введите x:");
         if (in.hasNextInt()) {
-            int a = in.nextInt();
-            out.println("Результат: " + Integer.toString(n.findFirst(array, a)));
+            int a41 = in.nextInt();
+            out.println("Результат: " + Integer.toString(n.findFirst(array41, a41)));
         } else out.println("Неверный ввод");
-        out.println("Исxодный массив: " + Arrays.toString(array));
-        */
-        // 4.3 ex
-        /*
+
+        //17(4.3)
         out.println("Наибольшее по модулю значение массива");
-        int[] array = new int[10];
-        Random rnd = new Random();
+        int[] array43 = new int[10];
         for (int i = 0; i<10; i++) {
-            array[i] = rnd.nextInt(21)-10;
+            array43[i] = rnd.nextInt(21)-10;
         }
-        out.println("Исxодный массив: " + Arrays.toString(array));
-        out.println("Результат: " + Integer.toString(n.maxAbs(array)));
-         */
-        // 4.5 ex
-        /*
-        out.println("Вставка элементов массива ins в массив arr\nВведите с какой позиции начать вставку:");
-        int[] arr1 = new int[10];
-        int[] arr2 = new int[10];
-        Random rnd = new Random();
-        for (int i = 0; i<10; i++) {
-            arr1[i] = rnd.nextInt(10)+1;
+        out.println("Исxодный массив: " + Arrays.toString(array43));
+        out.println("Результат: " + Integer.toString(n.maxAbs(array43)));
+
+        //18(4.5)
+        out.println("Вставка элементов массива ins в массив arr");
+        int[] arr45 = new int[5];
+        int[] ins45 = new int[5];
+        for (int i = 0; i<5; i++) {
+            arr45[i] = rnd.nextInt(10)+1;
         }
-        for (int i = 0; i<10; i++) {
-            arr2[i] = rnd.nextInt(10)+1;
+        for (int i = 0; i<5; i++) {
+            ins45[i] = rnd.nextInt(10)+1;
         }
+        out.println("Исxодный массив arr: " + Arrays.toString(arr45));
+        out.println("Исxодный массив ins: " + Arrays.toString(ins45));
+        out.println("Введите с какой позиции начать вставку:");
         int k;
-        if (in.hasNextInt() && ((k = in.nextInt()) > 0)) {
-            out.println("Исxодный массив arr: " + Arrays.toString(arr1));
-            out.println("Исxодный массив ins: " + Arrays.toString(arr2));
-            out.println("Результат: " + Arrays.toString(n.add(arr1, arr2, k)));
+        if (in.hasNextInt() && ((k = in.nextInt()) >= 0)) {
+            out.println("Результат: " + Arrays.toString(n.add(arr45, ins45, k)));
         } else out.println("Неверный ввод");
-         */
-        // 4.7 ex
-        /*
-        out.println("Новый массив, в котором значения исходного массива записаны задом наперед.");
-        int[] array = new int[10];
-        Random rnd = new Random();
+
+        //19(4.7)
+        out.println("Возвратный реверс\nСоздание нового массива, в котором значения исходного массива записаны задом наперед");
+        int[] array47 = new int[10];
         for (int i = 0; i<10; i++) {
-            array[i] = rnd.nextInt(21)-10;
+            array47[i] = rnd.nextInt(21)-10;
         }
-        out.println("Исxодный массив: " + Arrays.toString(array));
-        out.println("Результат: " + Arrays.toString(n.reverseBack(array)));
-         */
-        // 4.9 ex
-        /*
-        out.println("Индексы всех вхождений числа x в массив\nВведите x:");
-        int[] arr = new int[10];
-        Random rnd = new Random();
+        out.println("Исxодный массив: " + Arrays.toString(array47));
+        out.println("Результат: " + Arrays.toString(n.reverseBack(array47)));
+
+        //20(4.9)
+        out.println("Индексы всех вхождений числа x в массив");
+        int[] arr49 = new int[10];
         for (int i = 0; i<10; i++) {
-            arr[i] = rnd.nextInt(10)+1;
+            arr49[i] = rnd.nextInt(10)+1;
         }
+        out.println("Исxодный массив: " + Arrays.toString(arr49));
+        out.println("Введите x:");
         if (in.hasNextInt()) {
-            int k = in.nextInt();
-            out.println("Исxодный массив " + Arrays.toString(arr));
-            out.println("Результат: " + Arrays.toString(n.findAll(arr, k)));
+            int k49 = in.nextInt();
+            out.println("Результат: " + Arrays.toString(n.findAll(arr49, k49)));
         } else out.println("Неверный ввод");
-
-         */
     }
-
+    //1(1.1)
     public double fraction(double x) {
         return (x % 1);
     }
-
+    //2(1.3)
     public int charToNum(char x) {
         return x - '0';
     }
-
+    //3(1.5)
     public boolean is2Digits(int x) {
-        if ((x / 10 > 0) && (x / 10 < 10)) {
+        if ((Math.abs(x / 10) > 0) && (Math.abs(x / 10) < 10)) {
             return true;
         } else {
             return false;
         }
     }
-
+    //4(1.7)
     public boolean isInRange(int a, int b, int num) {
         if (a > b) {
             if ((num <= a) && (num >= b)) {
@@ -230,7 +245,7 @@ public class Main {
         }
 
     }
-
+    //5(1.9)
     public boolean isEqual(int a, int b, int c) {
         if ((a == b) && (b == c)) {
             return true;
@@ -238,7 +253,7 @@ public class Main {
             return false;
         }
     }
-
+    //6(2.1)
     public int abs(int x) {
         if (x < 0) {
             return (x * (-1));
@@ -246,7 +261,7 @@ public class Main {
             return x;
         }
     }
-
+    //7(2.3)
     public boolean is35(int x) {
         if (((x % 3 == 0) && (x % 5 == 0)) || ((x % 3 != 0) && (x % 5 != 0))) {
             return false;
@@ -254,7 +269,7 @@ public class Main {
             return true;
         }
     }
-
+    //8(2.5)
     public int max3(int x, int y, int z) {
         int max;
         if (x > y) max = x;
@@ -262,13 +277,13 @@ public class Main {
         if (max > z) return max;
         else return z;
     }
-
+    //9(2.7)
     public int sum2(int x, int y) {
         if ((x + y >= 10) && (x + y <= 19)) return 20;
         else return (x + y);
 
     }
-
+    //10(2.9)
     public String day(int x) {
         String result;
         switch (x) {
@@ -299,7 +314,7 @@ public class Main {
         }
         return result;
     }
-
+    //11(3.1)
     public String listNums (int x) {
         String res ="0";
         for (int i=1; i<=x; i++) {
@@ -307,7 +322,7 @@ public class Main {
         }
         return res;
     }
-
+    //12(3.3)
     public String chet (int x) {
         String res ="0";
         for (int i=2; i<=x; i=i+2) {
@@ -315,7 +330,7 @@ public class Main {
         }
         return res;
     }
-
+    //13(3.5)
     public int numLen (long x) {
         int length = 1;
         while (x/10 != 0) {
@@ -324,7 +339,7 @@ public class Main {
         }
         return length;
     }
-
+    //14(3.7)
     public void square (int x) {
         for (int i = 0; i<x; i++) {
             for (int k = 1; k<x; k++) {
@@ -333,7 +348,7 @@ public class Main {
             out.println('*');
         }
     }
-
+    //15(3.9)
     public void rightTriangle (int x) {
         int star = 1;
         int gap = x-1;
@@ -349,16 +364,16 @@ public class Main {
             out.println();
         }
     }
-
+    //16(4.1)
     public int findFirst (int[] arr, int x) {
         int index = 0;
-        while ((arr[index]!=x) && (index<arr.length-1)) {
+        while ((index<arr.length)&&(arr[index]!=x)) {
             index++;
         }
-        if (index<arr.length-1)  return index;
-        else return -1;
+        if (index==arr.length)  return -1;
+        else return index;
     }
-
+    //17(4.3)
     public int maxAbs (int[] arr) {
         int max=0;
         int index=1;
@@ -368,7 +383,7 @@ public class Main {
         }
         return arr[max];
     }
-
+    //18(4.5)
     public int[] add (int[] arr, int[] ins, int pos) {
         int[] newAr = new int[arr.length + ins.length];
         int index = 0;
@@ -389,7 +404,7 @@ public class Main {
         }
         return newAr;
     }
-
+    //19(4.7)
     public int[] reverseBack (int[] arr) {
         int[] newArr = new int[arr.length];
         int ind = 0;
@@ -399,7 +414,7 @@ public class Main {
         }
         return newArr;
     }
-
+    //20(4.9)
     public int[] findAll (int[] arr, int x) {
         int sum=0;
         for (int i = 0; i < arr.length; i++) {
@@ -415,5 +430,4 @@ public class Main {
         }
         return newArr;
     }
-
 }
